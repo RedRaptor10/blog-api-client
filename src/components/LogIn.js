3be19bd6d6c29e7a682f67be9be2e1032c89f3bd
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-17 04:29:40 -0500

    Added log in processing and form errors

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -6,6 +6,8 @@ const LogIn = () => {
         password: ''
     });
 
+    const [formErrors, setFormErrors] = useState([]);
+
     const handleChange = event => {
         setForm({
             ...form,
@@ -28,17 +30,28 @@ const LogIn = () => {
 
         fetch('http://localhost:3000/api/login', options)
         .then(function(res) { return res.json(); })
-        .then(function(res) { console.log(res); });
+        .then(function(res) {
+            if (res.errors) { setFormErrors(res.errors); } // Username/password required
+            else if (!res.user) { setFormErrors([{ msg: res.info.message }]); } // Incorrect username/password
+            else { window.location.href = '/' } // Success, redirect to Home page
+        });
     };
 
 	return(
 		<main id="log-in">
-            <form action="">
+            <form id="log-in-form" action="">
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" onChange={handleChange}></input>
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" onChange={handleChange}></input>
                 <button type="submit" name="submit" onClick={submitForm}>Log In</button>
+                {formErrors.length !== 0 ?
+                    formErrors.map((formError, i) => {
+                        return(
+                            <div key={i}>{formError.msg}</div>
+                        )
+                    })
+                : null}
             </form>
 		</main>
 	);
