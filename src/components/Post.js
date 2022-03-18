78fca5760743596cc05be22ab804a37267139a05
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-18 13:06:35 -0500

    Added deleteComment function

--- src/components/Post.js
+++ src/components/Post.js
@@ -83,6 +83,43 @@ const Post = ({user, setUser, getCookie, deleteCookie}) => {
         });;
     };
 
+    const deleteComment = commentId => {
+        let token = getCookie('blog_api_token');
+        // If no token then unset user, delete cookie, and exit
+        if (token === '') {
+            setUser();
+            deleteCookie('blog_api_token');
+            return;
+        }
+
+        const options = {
+            method: 'POST',
+            headers: { 'Authorization': 'Bearer ' + token },
+            mode: 'cors'
+        };
+
+        fetch('http://localhost:3000/api/posts/' + postId + '/comments/' + commentId + '/delete', options)
+        .then(function(res) {
+            // If unauthorized then unset user, delete cookie, and throw error
+            if (res.statusText === 'Unauthorized') {
+                setUser();
+                deleteCookie('blog_api_token');
+                throw new Error(res.statusText);
+            } else {
+                return res.json();
+            }
+        })
+        .then(function(res) {
+            // Success. Fetch comments again.
+            fetch('http://localhost:3000/api/posts/' + postId + '/comments?sort=date&order=desc', {mode: 'cors'})
+            .then(function(res) { return res.json(); })
+            .then(function(res) { setComments(res); });
+        })
+        .catch(err => {
+            console.log(err.message);
+        });;
+    }
+
 	return(
 		<main id="post">
             {post && post.author ?
@@ -116,6 +153,9 @@ const Post = ({user, setUser, getCookie, deleteCookie}) => {
                                         <div>by <Link to={'/users/' + comment.author.username}>{comment.author.username}</Link></div>
                                         <div>{formatDate(comment.date)}</div>
                                         <div>{comment.content}</div>
+                                        {user && user.username === comment.author.username ?
+                                            <div onClick={() => { deleteComment(comment._id); }}>Delete</div>
+                                        : null}
                                     </div>
                                 )
                             })
