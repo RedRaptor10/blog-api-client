640348d7450dc398ebeae4220a903afcdabf958c
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-18 13:19:53 -0500

    Created Comment component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import formatDate from '../helpers/formatDate.js';
+import Comment from "./Comment";
 
 const Post = ({user, setUser, getCookie, deleteCookie}) => {
     const { postId } = useParams(); // Get post id from url
@@ -83,43 +84,6 @@ const Post = ({user, setUser, getCookie, deleteCookie}) => {
         });;
     };
 
-    const deleteComment = commentId => {
-        let token = getCookie('blog_api_token');
-        // If no token then unset user, delete cookie, and exit
-        if (token === '') {
-            setUser();
-            deleteCookie('blog_api_token');
-            return;
-        }
-
-        const options = {
-            method: 'POST',
-            headers: { 'Authorization': 'Bearer ' + token },
-            mode: 'cors'
-        };
-
-        fetch('http://localhost:3000/api/posts/' + postId + '/comments/' + commentId + '/delete', options)
-        .then(function(res) {
-            // If unauthorized then unset user, delete cookie, and throw error
-            if (res.statusText === 'Unauthorized') {
-                setUser();
-                deleteCookie('blog_api_token');
-                throw new Error(res.statusText);
-            } else {
-                return res.json();
-            }
-        })
-        .then(function(res) {
-            // Success. Fetch comments again.
-            fetch('http://localhost:3000/api/posts/' + postId + '/comments?sort=date&order=desc', {mode: 'cors'})
-            .then(function(res) { return res.json(); })
-            .then(function(res) { setComments(res); });
-        })
-        .catch(err => {
-            console.log(err.message);
-        });;
-    }
-
 	return(
 		<main id="post">
             {post && post.author ?
@@ -148,15 +112,8 @@ const Post = ({user, setUser, getCookie, deleteCookie}) => {
                         {comments.length !== 0 ?
                             comments.map(comment => {
                                 return(
-                                    <div className="comment" key={comment._id}>
-                                        <div>{comment.title}</div>
-                                        <div>by <Link to={'/users/' + comment.author.username}>{comment.author.username}</Link></div>
-                                        <div>{formatDate(comment.date)}</div>
-                                        <div>{comment.content}</div>
-                                        {user && user.username === comment.author.username ?
-                                            <div onClick={() => { deleteComment(comment._id); }}>Delete</div>
-                                        : null}
-                                    </div>
+                                    <Comment key={comment._id} user={user} setUser={setUser} comment={comment} comments={comments} setComments={setComments}
+                                        getCookie={getCookie} deleteCookie={deleteCookie} />
                                 )
                             })
                         : null}
