5385978d73c9254e5a95014960625c1b49a29395
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-19 03:32:02 -0500

    Separated cookie functions

--- src/components/App.js
+++ src/components/App.js
@@ -8,68 +8,40 @@ import Footer from "./Footer";
 import User from "./User";
 import UserEdit from "./UserEdit";
 import Post from "./Post";
+import { getCookie, deleteCookie } from "../helpers/cookies.js";
 
 const App = () => {
 	const [user, setUser] = useState();
 
-	// Get cookie from string: document.cookie == 'cookie1=value; cookie2=value;'
-	const getCookie = cname => {
-		let name = cname + '=';
-		let decodedCookie = decodeURIComponent(document.cookie); // Decode document.cookie to handle cookies with special characters, e.g. '$'
-		let cookieArray = decodedCookie.split(';');
-		// Loop through cookieArray and read each cookie
-		for (let i = 0; i < cookieArray.length; i++) {
-			let c = cookieArray[i];
-			while (c.charAt(0) === ' ') { c = c.substring(1); }
-			// If cookie is found, return the cookie value
-			if (c.indexOf(name) === 0) { return c.substring(name.length, c.length); }
-		}
-		// Cookie not found, return empty string
-		return '';
-	};
-
-	// Delete cookie by setting expires parameter to past date (NOTE: Cookie value does not have to be specified)
-	const deleteCookie = cname => {
-		document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=Lax; path=/';
-	}
-
 	// Check authorization on componentDidMount
 	useEffect(() => {
-		const checkAuth = () => {
-			let token = getCookie('blog_api_token');
-			// If no token then unset user, delete cookie, and exit
-			if (token === '') {
-				setUser();
-				deleteCookie('blog_api_token');
-				return;
-			}
-	
-			const options = {
-				method: 'GET',
-				headers: { 'Authorization': 'Bearer ' + token },
-				mode: 'cors'
-			};
-	
-			fetch('http://localhost:3000/api/auth', options)
-			.then(function(res) {
-				// If unauthorized then unset user, delete cookie, and throw error
-				if (res.statusText === 'Unauthorized') {
-					setUser();
-					deleteCookie('blog_api_token');
-					throw new Error(res.statusText);
-				} else {
-					return res.json();
-				}
-			})
-			.then(function(res) {
-				setUser(res);
-			})
-			.catch(err => {
-				console.log(err.message);
-			});
+		let token = getCookie('blog_api_token');
+		// If no token then unset user, delete cookie, and exit
+		if (token === '') {
+			setUser();
+			deleteCookie('blog_api_token');
+			return;
+		}
+
+		const options = {
+			method: 'GET',
+			headers: { 'Authorization': 'Bearer ' + token },
+			mode: 'cors'
 		};
 
-		checkAuth();
+		fetch('http://localhost:3000/api/auth', options)
+		.then(function(res) {
+			// If unauthorized then unset user, delete cookie, and throw error
+			if (res.statusText === 'Unauthorized') {
+				setUser();
+				deleteCookie('blog_api_token');
+				throw new Error(res.statusText);
+			} else {
+				return res.json();
+			}
+		})
+		.then(function(res) { setUser(res); })
+		.catch(err => { console.log(err.message); });
 	}, []);
 
 	return(
@@ -80,8 +52,8 @@ const App = () => {
 				<Route exact path="/login" element={<LogIn />} />
 				<Route exact path="/signup" element={<SignUp />} />
 				<Route exact path="/users/:username" element={<User user={user} />} />
-				<Route exact path="/users/:username/edit" element={<UserEdit user={user} setUser={setUser} getCookie={getCookie} deleteCookie={deleteCookie} />} />
-				<Route exact path="/posts/:postId" element={<Post user={user} setUser={setUser} getCookie={getCookie} deleteCookie={deleteCookie} />} />
+				<Route exact path="/users/:username/edit" element={<UserEdit user={user} setUser={setUser} />} />
+				<Route exact path="/posts/:postId" element={<Post user={user} setUser={setUser} />} />
 			</Routes>
 			<Footer />
 		</HashRouter>
